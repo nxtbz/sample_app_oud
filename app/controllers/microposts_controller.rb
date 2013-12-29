e9a5b7f05d167c41f1547502fcceece809d41276
do0fusz <do0fusz@hotmail.com>
2013-12-29 17:45:44 +0100

    excercis

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,11 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :signed_in_user, only: [:create, :destroy]
   before_action :correct_user,   only: :destroy
- 
-  def destroy
-    @micropost.destroy
-    redirect_to root_url
-  end
+  
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -18,7 +14,10 @@ class MicropostsController < ApplicationController
     end
   end
 
-
+  def destroy
+    @micropost.destroy
+    redirect_to root_url
+  end
 
   private
 
@@ -28,7 +27,12 @@ class MicropostsController < ApplicationController
 
     def correct_user
       @micropost = current_user.microposts.find_by(id: params[:id])
-      redirect_to root_url if @microposts.nil?
+      redirect_to root_url if @micropost.nil?
     end
+
 end
+
+
+
+
 
